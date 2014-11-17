14c9de6b967c73348ced7eef9423952cd71412ae
miya-masa <miyauchi0623@gmail.com>
2014-11-17 13:44:56 +0900

    commit!

--- dotFilesLink.bat
+++ dotFilesLink.bat
@@ -1,6 +1,25 @@
-cd C:\Users\%USERNAME%
 
-mklink .\_vimrc .\dotFiles\_vimrc 
-mklink .\_gvimrc .\dotFiles\_gvimrc 
-mklink /D .\.vim .\dotFiles\.vim 
+@echo off
+
+rem
+rem このバッチの説明
+rem
+
+rem 設定事項
+set HOGE="変数の値"
+
+rem このバッチが存在するフォルダをカレントに
+pushd %0\..
+cls
+
+cd C:\Users\%USERNAME%
+del .\_vimrc
+mklink .\_vimrc .\dotfiles\_vimrc 
+del .\_gvimrc
+mklink .\_gvimrc .\dotfiles\_gvimrc 
+rd .\.vim
+mklink /D .\.vim .\dotfiles\.vim 
+pushd %0\..
+
 pause
+exit
