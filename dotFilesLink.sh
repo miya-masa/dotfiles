83312e2ba85d51cf69c6ae3fda40f5f548dcc868
miya-masa <miyauchi0623@gmail.com>
2014-12-17 18:45:19 +0900

    dotFile -> dotfile

--- dotFilesLink.sh
+++ dotFilesLink.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
 
-ln -s ~/dotFiles/_vimrc ~/.vimrc
-ln -s ~/dotFiles/_gvimrc ~/.gvimrc
-ln -s ~/dotFiles/.gitconfig ~/.gitconfig
+ln -s ~/dotfiles/_vimrc ~/.vimrc
+ln -s ~/dotfiles/_gvimrc ~/.gvimrc
+ln -s ~/dotfiles/.gitconfig ~/.gitconfig
 curl https://raw.githubusercontent.com/Shougo/neobundle.vim/master/bin/install.sh | sh
 mkdir -p ~/.vim/bundle
 git clone https://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
