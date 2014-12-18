0a883de76c9835d531f7843c0cb4721b5090cf6f
miyauchi0623@gmail.com <miyauchi0623@gmail.com>
2014-12-18 06:49:31 +0000

    commit!

--- dotFilesLink.sh
+++ dotFilesLink.sh
@@ -3,6 +3,5 @@
 ln -s ~/dotfiles/_vimrc ~/.vimrc
 ln -s ~/dotfiles/_gvimrc ~/.gvimrc
 ln -s ~/dotfiles/.gitconfig ~/.gitconfig
-curl https://raw.githubusercontent.com/Shougo/neobundle.vim/master/bin/install.sh | sh
 mkdir -p ~/.vim/bundle
 git clone https://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
