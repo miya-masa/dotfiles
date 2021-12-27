5f561244a210e07639c918b2884b6603829194d0
miya-masa <miyauchi0623@gmail.com>
2021-12-27 16:33:07 +0900

    fix

--- install.sh
+++ install.sh
@@ -11,6 +11,7 @@ __ScriptVersion="v0.1.0"
 UNAME=$(uname)
 DOTFILES_DIRECTORY=${DOTFILES_DIRECTORY:-~/dotfiles}
 DOTFILES_BRANCH=${DOTFILES_BRANCH:-master}
+XDG_CONFIG_HOME=${XDG_CONFIG_HOME}
 
 #===  FUNCTION  ================================================================
 #         NAME:  usage
