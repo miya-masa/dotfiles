df5a428474622de955e7ec9ed6d6d93933b0dd58
miya-masa <miyauchi0623@gmail.com>
2021-12-28 10:21:38 +0900

    install brew

--- etc/install.d/0004_brew_bundle.sh
+++ etc/install.d/0004_brew_bundle.sh
@@ -7,4 +7,7 @@ set -o errtrace
 set -o pipefail
 IFS=$'\n\t'
 
+if ! has brew ; then
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+fi
 brew bundle --file=./Brewfile_linux
