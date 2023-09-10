1ae91976cc9a216151c8ec7b6f5e7bcce182c3a5
miya-masa <miyauchi0623@gmail.com>
2023-09-10 14:21:28 +0000

    chore(*): lint

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -603,11 +603,6 @@ mason_lspconfig.setup_handlers {
 -- [[ Configure nvim-cmp ]]
 -- See `:help cmp`
 local cmp = require 'cmp'
-local has_words_before = function()
-  unpack = unpack or table.unpack
-  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
-end
 
 local feedkey = function(key, mode)
   vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(key, true, true, true), mode, true)
@@ -658,7 +653,7 @@ cmp.setup {
 
       -- The function below will be called before any actual modifications from lspkind
       -- so that you can provide more controls on popup customization. (See [#30](https://github.com/onsails/lspkind-nvim/pull/30))
-      before = function(entry, vim_item)
+      before = function(_, vim_item)
         return vim_item
       end
     })
