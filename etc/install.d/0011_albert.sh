b6f2784a19371e36852bef7cb7046e97cfa95251
miya-masa <miyauchi0623@gmail.com>
2024-03-07 08:14:57 +0000

    update

--- etc/install.d/0011_albert.sh
+++ etc/install.d/0011_albert.sh
@@ -11,6 +11,6 @@ if ! has albert ; then
   echo "Install albert"
   echo 'deb http://download.opensuse.org/repositories/home:/manuelschneid3r/xUbuntu_22.04/ /' | sudo tee /etc/apt/sources.list.d/home:manuelschneid3r.list
   curl -fsSL https://download.opensuse.org/repositories/home:manuelschneid3r/xUbuntu_22.04/Release.key | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/home_manuelschneid3r.gpg > /dev/null
-  sudo apt update
-  sudo apt install albert
+  sudo apt update -y
+  sudo apt install albert -y
 fi
