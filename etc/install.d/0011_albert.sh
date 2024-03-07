c3c36693955e36f70ae7e11da0a7f89869a7e51a
miya-masa <miyauchi0623@gmail.com>
2024-03-07 08:13:22 +0000

    albert

--- etc/install.d/0011_albert.sh
+++ etc/install.d/0011_albert.sh
@@ -9,13 +9,8 @@ IFS=$'\n\t'
 
 if ! has albert ; then
   echo "Install albert"
-
-  sudo rpm --import "https://build.opensuse.org/projects/home:manuelschneid3r/public_key"
-  curl https://build.opensuse.org/projects/home:manuelschneid3r/public_key | sudo apt-key add -
-  UBUNTU_VERSION=20.04
-  echo "deb http://download.opensuse.org/repositories/home:/manuelschneid3r/xUbuntu_${UBUNTU_VERSION}/ /" | sudo tee /etc/apt/sources.list.d/home:manuelschneid3r.list
-  sudo wget -nv https://download.opensuse.org/repositories/home:manuelschneid3r/xUbuntu_${UBUNTU_VERSION}/Release.key -O "/etc/apt/trusted.gpg.d/home:manuelschneid3r.asc"
-  sudo apt update -y
-  sudo apt install -y albert
+  echo 'deb http://download.opensuse.org/repositories/home:/manuelschneid3r/xUbuntu_22.10/ /' | sudo tee /etc/apt/sources.list.d/home:manuelschneid3r.list
+  curl -fsSL https://download.opensuse.org/repositories/home:manuelschneid3r/xUbuntu_22.10/Release.key | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/home_manuelschneid3r.gpg > /dev/null
+  sudo apt update
+  sudo apt install albert
 fi
-
