d65945f7ad43989157d11ef3d8060ed59e4b6334
miya-masa <miyauchi0623@gmail.com>
2024-08-21 13:58:59 +0000

    update

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -514,4 +514,32 @@ return {
       },
     },
   },
+  {
+    'MagicDuck/grug-far.nvim',
+    config = function()
+      require('grug-far').setup {}
+    end,
+  },
+  {
+    'ThePrimeagen/refactoring.nvim',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      'nvim-treesitter/nvim-treesitter',
+    },
+    config = function()
+      require('refactoring').setup()
+      vim.keymap.set('x', '<leader>re', ':Refactor extract ')
+      vim.keymap.set('x', '<leader>rf', ':Refactor extract_to_file ')
+      vim.keymap.set('x', '<leader>rv', ':Refactor extract_var ')
+      vim.keymap.set({ 'n', 'x' }, '<leader>ri', ':Refactor inline_var')
+      vim.keymap.set('n', '<leader>rI', ':Refactor inline_func')
+      vim.keymap.set('n', '<leader>rb', ':Refactor extract_block')
+      vim.keymap.set('n', '<leader>rbf', ':Refactor extract_block_to_file')
+    end,
+  },
+  {
+    'MeanderingProgrammer/render-markdown.nvim',
+    opts = {},
+    dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
+  },
 }
