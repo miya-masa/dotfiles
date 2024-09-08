ca72664715de5c338d37b4918391daca5dfc951c
miya-masa <miyauchi0623@gmail.com>
2024-09-08 08:35:32 +0000

    update

--- etc/install.d/0002_mise.sh
+++ etc/install.d/0002_mise.sh
@@ -7,12 +7,11 @@ set -o errtrace
 set -o pipefail
 IFS=$'\n\t'
 
-[ -f /home/linuxbrew/.linuxbrew/bin/brew ] && eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)
-# curl https://mise.run | sh | sh
-# if ! has mise; then
-#   export PATH="$HOME/.local/bin:$PATH"
-#   __shell_name=$(basename "$SHELL")
-#   eval "$(~/.local/bin/mise activate "${__shell_name}")"
-# fi
+curl https://mise.run | sh | sh
+if ! has mise; then
+  export PATH="$HOME/.local/bin:$PATH"
+  __shell_name=$(basename "$SHELL")
+  eval "$(~/.local/bin/mise activate "${__shell_name}")"
+fi
 mise --version
-mise install -y
+mise install -y || true
