8ea1a51a59b30c48b9821daec9ced48a709f06c3
miya-masa <miyauchi0623@gmail.com>
2024-09-08 22:19:09 +0000

    update

--- etc/install.d/0002_mise.sh
+++ etc/install.d/0002_mise.sh
@@ -7,8 +7,8 @@ set -o errtrace
 set -o pipefail
 IFS=$'\n\t'
 
-export LDFLAGS="-L$(brew --prefix openssl@1.1)/lib $LDFLAGS"
-export CPPFLAGS="-I$(brew --prefix openssl@1.1)/include $CPPFLAGS"
+export LDFLAGS="-L$(brew --prefix openssl@1.1)/lib"
+export CPPFLAGS="-I$(brew --prefix openssl@1.1)/include"
 export PKG_CONFIG_PATH="$(brew --prefix openssl@1.1)/lib/pkgconfig"
 
 curl https://mise.run | sh
