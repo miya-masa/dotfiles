775785dc90ea4f5d64103c2cafa9ae9f2036e572
miya-masa <miyauchi0623@gmail.com>
2024-09-10 21:41:32 +0900

    improvement

--- etc/install.d/0006_docker.sh
+++ etc/install.d/0006_docker.sh
@@ -7,7 +7,35 @@ set -o errtrace
 set -o pipefail
 IFS=$'\n\t'
 
+function has() {
+  type "$1" >/dev/null 2>&1
+}
+
+# sudo コマンドをラップする関数
+sudo_wrap() {
+  if [ -n "${SUDO_ASKPASS:-}" ]; then
+    sudo -A "$@"
+  else
+    sudo "$@"
+  fi
+}
+
 if ! has docker; then
-  curl -fsSL https://get.docker.com | sh || true
-  curl -fsSL https://get.docker.com/rootless | sh || true
+  # Add Docker's official GPG key:
+  sudo_wrap apt-get update -y
+  sudo_wrap apt-get install ca-certificates curl -y
+  sudo_wrap install -m 0755 -d /etc/apt/keyrings
+  sudo_wrap curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+  sudo_wrap chmod a+r /etc/apt/keyrings/docker.asc
+
+  # Add the repository to Apt sources:
+  echo \
+    "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+    $(. /etc/os-release && echo "$VERSION_CODENAME") stable" |
+    sudo_wrap tee /etc/apt/sources.list.d/docker.list >/dev/null
+  sudo_wrap apt-get update -y
+  sudo_wrap apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+  sudo_wrap groupadd docker || true
+  sudo_wrap usermod -aG docker $USER || true
+  newgrp docker || true
 fi
