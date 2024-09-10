0d8a8308c076919c6772da959ea81ee082df42b4
miya-masa <miyauchi0623@gmail.com>
2024-09-10 22:05:16 +0900

    improvement

--- init.lua
+++ init.lua
@@ -769,9 +769,6 @@ require('lazy').setup({
       require('mason-lspconfig').setup {
         handlers = {
           function(server_name)
-            if server_name == 'tsserver' then
-              server_name = 'ts_ls'
-            end
             local server = servers[server_name] or {}
             -- This handles overriding only values explicitly passed
             -- by the server configuration above. Useful when disabling
