443d8011efd3947c067aaaa3891937b85911765a
miya-masa <miyauchi0623@gmail.com>
2025-12-04 09:45:37 +0900

    feat(nvim): add edgy.nvim for managing floating windows and splits feat(nvim): add helm-ls.nvim for Helm language server support feat(nvim): add opencode.nvim for AI-powered code generation and refactoring feat(nvim): add chezmoi extra plugin for managing dotfiles fix(snacks): disable animation for snacks.nvim to improve performance feat(zsh): add `awsprofile` function to select AWS profile using fzf

---
+++ dot_config/nvim/lua/plugins/edgy.lua
@@ -0,0 +1,100 @@
+return {
+  {
+    "folke/edgy.nvim",
+    opts = function()
+      local opts = {
+        bottom = {
+          {
+            ft = "toggleterm",
+            size = { height = 0.4 },
+            filter = function(buf, win)
+              return vim.api.nvim_win_get_config(win).relative == ""
+            end,
+          },
+          {
+            ft = "noice",
+            size = { height = 0.4 },
+            filter = function(buf, win)
+              return vim.api.nvim_win_get_config(win).relative == ""
+            end,
+          },
+          "Trouble",
+          { ft = "qf", title = "QuickFix" },
+          {
+            ft = "help",
+            size = { height = 20 },
+            -- don't open help files in edgy that we're editing
+            filter = function(buf)
+              return vim.bo[buf].buftype == "help"
+            end,
+          },
+          { title = "Spectre", ft = "spectre_panel", size = { height = 0.4 } },
+          { title = "Neotest Output", ft = "neotest-output-panel", size = { height = 15 } },
+        },
+        left = {
+          { title = "Neotest Summary", ft = "neotest-summary" },
+          -- "neo-tree",
+        },
+        right = {
+          { title = "Grug Far", ft = "grug-far", size = { width = 0.4 } },
+          { title = "Aerial", ft = "aerial", pinned = true, open = "AerialOpen" },
+        },
+        keys = {
+          -- increase width
+          ["<S-Right>"] = function(win)
+            win:resize("width", 2)
+          end,
+          -- decrease width
+          ["<S-Left>"] = function(win)
+            win:resize("width", -2)
+          end,
+          -- increase height
+          ["<S-Up>"] = function(win)
+            win:resize("height", 2)
+          end,
+          -- decrease height
+          ["<S-Down>"] = function(win)
+            win:resize("height", -2)
+          end,
+        },
+        options = {
+          animate = { enabled = false },
+          right = { size = 0.3 },
+          left = { size = 0.2 },
+        },
+      }
+
+      -- trouble
+      for _, pos in ipairs({ "top", "bottom", "left", "right" }) do
+        opts[pos] = opts[pos] or {}
+        table.insert(opts[pos], {
+          ft = "trouble",
+          filter = function(_buf, win)
+            return vim.w[win].trouble
+              and vim.w[win].trouble.position == pos
+              and vim.w[win].trouble.type == "split"
+              and vim.w[win].trouble.relative == "editor"
+              and not vim.w[win].trouble_preview
+          end,
+        })
+      end
+
+      -- snacks terminal
+      for _, pos in ipairs({ "top", "bottom", "left", "right" }) do
+        opts[pos] = opts[pos] or {}
+        table.insert(opts[pos], {
+          ft = "snacks_terminal",
+          size = { height = 0.4 },
+          title = "%{b:snacks_terminal.id}: %{b:term_title}",
+          filter = function(_buf, win)
+            return vim.w[win].snacks_win
+              and vim.w[win].snacks_win.position == pos
+              and vim.w[win].snacks_win.relative == "editor"
+              and not vim.w[win].trouble_preview
+          end,
+        })
+      end
+      return opts
+    end,
+  },
+}
--- dot_config/nvim/lua/plugins/snacks.lua
+++ dot_config/nvim/lua/plugins/snacks.lua
@@ -2,6 +2,9 @@ return {
   {
     "snacks.nvim",
     opts = {
+      animate = {
+        enabled = false,
+      },
       scroll = {
         animate = {
           duration = { step = 15, total = 25 },
